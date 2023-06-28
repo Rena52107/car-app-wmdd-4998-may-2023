fed84bf3ac6b703d245cbdd9d69fdb74a8554d23
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-27 21:48:34 -0700

    Set up server side

---
+++ server/src/peopleSchema.js
@@ -0,0 +1,17 @@
+const people = [
+  {
+    id: '1',
+    firstName: 'Bill',
+    lastName: 'Gates'
+  },
+  {
+    id: '2',
+    firstName: 'Steve',
+    lastName: 'Jobs'
+  },
+  {
+    id: '3',
+    firstName: 'Linux',
+    lastName: 'Torvalds'
+  }
+]

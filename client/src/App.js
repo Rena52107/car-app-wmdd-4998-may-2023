deb7e693ad716dc1bca91b9d1f27419f93fec5e7
Rena Sakata <rena.sakata.cn@gmail.com>
2023-06-28 20:51:06 -0700

    Add react router

--- client/src/App.js
+++ client/src/App.js
@@ -2,22 +2,21 @@ import "antd/dist/reset.css";
 import "./App.css";
 
 import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
-import Title from "./components/layout/Title";
-import AddPerson from "./components/forms/AddPerson";
-import People from "./components/lists/People";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Root from "./Root";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/graphql",
   cache: new InMemoryCache(),
 });
 
+const router = createBrowserRouter([{ path: "*", Component: Root }]);
+
 function App() {
   return (
     <ApolloProvider client={client}>
       <div className="App">
-        <Title />
-        <AddPerson />
-        <People/>
+        <RouterProvider router={router} />
       </div>
     </ApolloProvider>
   );
